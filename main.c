737c978787bd38c12aa91997bcfaed560183f0d4
iskhakovay <rina07rovel@gmail.com>
2023-05-10 16:05:27 +0300

    main v3

--- main.c
+++ main.c
@@ -13,7 +13,6 @@
 #define CHUNK_SIZE 256
 
 
-bool checkInputFiles(char** argv, int argc);
 int main(int argc, char** argv)//maybe char* argv
 {
     bool capslock;
@@ -23,44 +22,48 @@ int main(int argc, char** argv)//maybe char* argv
     FILE *target;
     FILE *queues;
     int option;
+    if( argc > 6 ) {
+        printf("Too many arguments supplied.\n");
+    }
     // put ':' at the starting of the string so compiler can distinguish between '?' and ':'
-    for(int i = 0; i<argc; i++) {
-        while ((option = getopt(argc, &argv[i], ":if:i")) != -1) { //get option from the getopt() method
-            switch (option) {
-                //For option i, r, l, print that these are options
-                case 'i':
-                    capslock = CAPSLOCK_OFF;
-                    break;
-                case 'f':
-                    if(i == 1){
-                        students = fopen(argv[1],"r");
-                        fclose(students);
-                    }
-                    if(i == 2){
-                        courses = fopen(argv[2],"r");
-                        fclose(courses);
-                    }
-                    if(i == 3){
-                        hackers = fopen(argv[3],"r");
-                        fclose(hackers);
-                    }
-                    if(i == 4){
-                        queues = fopen(argv[4],"r");
-                        fclose(queues);
-                    }
-                    if(i == 5){
-                        target = fopen(argv[5],"r");
-                        fclose(target);
-                    }
+    int i =0;
+    while ((option = getopt(argc, argv, ":if:i")) != -1) { //get option from the getopt() method
+        switch (option) {
+            //For option i, r, l, print that these are options
+            case 'i':
+                capslock = CAPSLOCK_OFF;
+                break;
+            case 'f':
+                if(i == 1){
+                    students = fopen(argv[1],"r");
+                    fclose(students);
+                }
+                if(i == 2){
+                    courses = fopen(argv[2],"r");
+                    fclose(courses);
+                }
+                if(i == 3){
+                    hackers = fopen(argv[3],"r");
+                    fclose(hackers);
+                }
+                if(i == 4){
+                    queues = fopen(argv[4],"r");
+                    fclose(queues);
+                }
+                if(i == 5){
+                    target = fopen(argv[5],"r");
+                    fclose(target);
+                }
 
-                    break;
-                case ':':
-                    break;
-                case '?': //used for some unknown options
-                    printf("unknown option: %c\n", optopt);
-                    break;
-            }
+                break;
+            case ':':
+                break;
+            case '?': //used for some unknown options
+                printf("unknown option: %c\n", optopt);
+                break;
         }
+        i++;
+
     }
     EnrollmentSystem sys =createEnrollment(students, courses, hackers);
     sys =readEnrollment(sys, queues);
@@ -69,23 +72,3 @@ int main(int argc, char** argv)//maybe char* argv
 
 
 }
-bool checkInputFiles(char** argv, int argc){ //checks files and returns if capslock true
-    FILE* file_handler;
-    if(argc>6){
-        printf("Too many arguments");
-    }
-    for(int i = 0; i < argc; i++) {
-        if ((((file_handler) = fopen(argv[i], "r")) == NULL)){
-            printf("No file supplied");
-        }
-        fclose(file_handler);
-    }
-    if(argc>5){
-        if(strcmp(argv[0],"-i")==0){
-            return CAPSLOCK_OFF;
-        }
-    }
-    return CAPSLOCK_ON;
-
-}
-
