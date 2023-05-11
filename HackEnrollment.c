fa5095ccc11c9bd77253bbe527e9694a4c542608
iskhakovay <rina07rovel@gmail.com>
2023-05-11 19:58:26 +0300

    israeli queue 6

--- HackEnrollment.c
+++ HackEnrollment.c
@@ -344,9 +344,9 @@ EnrollmentSystem readEnrollment(EnrollmentSystem sys, FILE* queues)
 int (**pt)(void*, void*);
 void hackEnrollment(EnrollmentSystem sys, FILE* out)
 {
-    (&pt)[0] = (&studentIdDifference); //TODO check pointers
-    (&pt)[1] =  (&nameDistance);
-    (&pt)[2] = (&checkHackerFileFriendStatus);
+    (&pt)[0] = (int (**)(void *, void *)) (&studentIdDifference); //TODO check pointers
+    (&pt)[1] = (int (**)(void *, void *)) (&nameDistance);
+    (&pt)[2] = (int (**)(void *, void *)) (&checkHackerFileFriendStatus);
 
     IsraeliQueue* arr_of_queues = malloc(sizeof(IsraeliQueue)*sys->num_of_ques);
 
